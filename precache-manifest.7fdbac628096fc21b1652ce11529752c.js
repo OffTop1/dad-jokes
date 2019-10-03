a534304ca9ef3da91eb36d1fcc51afbc134c2fa8
OffTop1 <1quyoshbekm@gmail.com>
2019-10-03 19:56:35 +0300

    Updates

---
+++ precache-manifest.7fdbac628096fc21b1652ce11529752c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63a1ed325a66cff41e36dd3e6e8cb0f4",
+    "url": "/dad-jokes/index.html"
+  },
+  {
+    "revision": "f43ff9a8cd556ce92470",
+    "url": "/dad-jokes/static/css/main.c14e5710.chunk.css"
+  },
+  {
+    "revision": "1d51efd6b9bac9dd86e3",
+    "url": "/dad-jokes/static/js/2.9b1c02a4.chunk.js"
+  },
+  {
+    "revision": "f43ff9a8cd556ce92470",
+    "url": "/dad-jokes/static/js/main.3762519e.chunk.js"
+  },
+  {
+    "revision": "29110c9c3fa1e833f200",
+    "url": "/dad-jokes/static/js/runtime~main.492fb1d7.js"
+  }
+]);
